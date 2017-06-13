88ab410c390b25d8be3da38f141c9faa08cda47f
SirCaptainMitch <hamann.mitchell@gmail.com>
2017-06-12 19:15:28 -0500

    review changes

--- utils/dbo.sp_FindQueryPlan.sql
+++ utils/dbo.sp_FindQueryPlan.sql
@@ -14,78 +14,79 @@ ALTER PROCEDURE [dbo].[sp_FindQueryPlan]
     @ObjectName NVARCHAR(128) = NULL  
 WITH RECOMPILE
 AS
-SET NOCOUNT ON;
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;    
+BEGIN 
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;    
 
-DECLARE @Sql NVARCHAR(MAX)	   
+	DECLARE @Sql NVARCHAR(MAX)	   
 
-IF OBJECT_ID('tempdb..#ProcCache') IS NOT NULL 
-	DROP TABLE #ProcCache 
+	IF OBJECT_ID('tempdb..#ProcCache') IS NOT NULL 
+		DROP TABLE #ProcCache 
 
-CREATE TABLE #ProcCache
-			( id INT IDENTITY(1,1) 
-			 ,ObjectId INT
-			 ,ObjectName VARCHAR(255)
-			 ,PlanHandle VARBINARY(64)
-			 ,QueryPlan XML 
-			 ,CachedTime DATETIME
-			 ,ExecCount INT 			 
-			 ,[LastElapsedTime(S)] NUMERIC(12,4)
-			 ,SetOptions VARCHAR(1000) 
-			) 
+	CREATE TABLE #ProcCache
+				( id INT IDENTITY(1,1) 
+				,ObjectId INT
+				,ObjectName VARCHAR(255)
+				,PlanHandle VARBINARY(64)
+				,QueryPlan XML 
+				,CachedTime DATETIME
+				,ExecCount INT 			 
+				,[LastElapsedTime(S)] NUMERIC(12,4)
+				,SetOptions VARCHAR(1000) 
+				) 
 
-SELECT @Sql =  
-N'USE ' + @DatabaseName + ' 
+	SELECT @Sql =  
+	N'USE ' + @DatabaseName + ' 
 
-DECLARE @ObjectId INT = OBJECT_ID(@ObjectName)
+	DECLARE @ObjectId INT = OBJECT_ID(@ObjectName)
 
-SELECT o.object_id
-	 , OBJECT_NAME(o.object_id) 
-	 , s.plan_handle
-	 , h.query_plan
-	 , s.cached_time
-	 , S.execution_count
-	 , CONVERT(NUMERIC(12,4),( CONVERT(NUMERIC,s.last_elapsed_time) / CONVERT(NUMERIC,1000000) )) 
-FROM sys.objects o 
-	 INNER JOIN sys.dm_exec_procedure_stats s on o.object_id = s.object_id
-	 CROSS APPLY sys.dm_exec_query_plan(s.plan_handle) h	 
-WHERE o.object_id = ISNULL(@ObjectId, o.object_id)'
+	SELECT o.object_id
+		, OBJECT_NAME(o.object_id) 
+		, s.plan_handle
+		, h.query_plan
+		, s.cached_time
+		, S.execution_count
+		, CONVERT(NUMERIC(12,4),( CONVERT(NUMERIC,s.last_elapsed_time) / CONVERT(NUMERIC,1000000) )) 
+	FROM sys.objects o 
+		INNER JOIN sys.dm_exec_procedure_stats s on o.object_id = s.object_id
+		CROSS APPLY sys.dm_exec_query_plan(s.plan_handle) h	 
+	WHERE o.object_id = ISNULL(@ObjectId, o.object_id)'
 
-INSERT #ProcCache 
-	  (  ObjectId
-	   , ObjectName
-	   , PlanHandle
-	   , QueryPlan
-	   , CachedTime
-	   , ExecCount
-	   , [LastElapsedTime(S)]) 
-EXEC sp_executesql @sql, N'@ObjectName VARCHAR(255)', @ObjectName
+	INSERT #ProcCache 
+		(  ObjectId
+		, ObjectName
+		, PlanHandle
+		, QueryPlan
+		, CachedTime
+		, ExecCount
+		, [LastElapsedTime(S)]) 
+	EXEC sp_executesql @sql, N'@ObjectName VARCHAR(255)', @ObjectName
 
--- This part taken from SQL First Responder Kit 
--- https://github.com/BrentOzarULTD/SQL-Server-First-Responder-Kit/blob/dev/sp_BlitzCache.sql#L2866
-UPDATE p 
-SET SetOptions = SUBSTRING(
-                    CASE WHEN (CAST(pa.value AS INT) & 1 = 1) THEN ', ANSI_PADDING' ELSE '' END +
-                    CASE WHEN (CAST(pa.value AS INT) & 8 = 8) THEN ', CONCAT_NULL_YIELDS_NULL' ELSE '' END +
-                    CASE WHEN (CAST(pa.value AS INT) & 16 = 16) THEN ', ANSI_WARNINGS' ELSE '' END +
-                    CASE WHEN (CAST(pa.value AS INT) & 32 = 32) THEN ', ANSI_NULLS' ELSE '' END +
-                    CASE WHEN (CAST(pa.value AS INT) & 64 = 64) THEN ', QUOTED_IDENTIFIER' ELSE '' END +
-                    CASE WHEN (CAST(pa.value AS INT) & 4096 = 4096) THEN ', ARITH_ABORT' ELSE '' END +
-                    CASE WHEN (CAST(pa.value AS INT) & 8192 = 8191) THEN ', NUMERIC_ROUNDABORT' ELSE '' END 
-                    , 2, 200000) 
-FROM #ProcCache p 
-	 CROSS APPLY sys.dm_exec_plan_attributes (p.PlanHandle) pa 
-WHERE  pa.attribute = 'set_options' 
+	-- This part taken from SQL First Responder Kit 
+	-- https://github.com/BrentOzarULTD/SQL-Server-First-Responder-Kit/blob/dev/sp_BlitzCache.sql#L2866
+	UPDATE p 
+	SET SetOptions = SUBSTRING(
+						CASE WHEN (CAST(pa.value AS INT) & 1 = 1) THEN ', ANSI_PADDING' ELSE '' END +
+						CASE WHEN (CAST(pa.value AS INT) & 8 = 8) THEN ', CONCAT_NULL_YIELDS_NULL' ELSE '' END +
+						CASE WHEN (CAST(pa.value AS INT) & 16 = 16) THEN ', ANSI_WARNINGS' ELSE '' END +
+						CASE WHEN (CAST(pa.value AS INT) & 32 = 32) THEN ', ANSI_NULLS' ELSE '' END +
+						CASE WHEN (CAST(pa.value AS INT) & 64 = 64) THEN ', QUOTED_IDENTIFIER' ELSE '' END +
+						CASE WHEN (CAST(pa.value AS INT) & 4096 = 4096) THEN ', ARITH_ABORT' ELSE '' END +
+						CASE WHEN (CAST(pa.value AS INT) & 8192 = 8191) THEN ', NUMERIC_ROUNDABORT' ELSE '' END 
+						, 2, 200000) 
+	FROM #ProcCache p 
+		CROSS APPLY sys.dm_exec_plan_attributes (p.PlanHandle) pa 
+	WHERE  pa.attribute = 'set_options' 
 
-SELECT ObjectId
-	   ,ObjectName 
-	   ,QueryPlan	   
-	   ,CachedTime
-	   ,ExecCount
-	   ,[LastElapsedTime(S)]
-	   ,[LastElapsedTime(S)] * CONVERT(NUMERIC(12,4), ExecCount) AS TotalExecTime
-	   ,SetOptions 
-FROM #ProcCache
-ORDER BY [LastElapsedTime(S)] * CONVERT(NUMERIC(12,4), ExecCount) DESC, [LastElapsedTime(S)] DESC 
-
-GO
+	SELECT ObjectId
+		,ObjectName 
+		,QueryPlan	   
+		,CachedTime
+		,ExecCount
+		,[LastElapsedTime(S)]
+		,[LastElapsedTime(S)] * CONVERT(NUMERIC(12,4), ExecCount) AS TotalExecTime
+		,SetOptions 
+	FROM #ProcCache
+	ORDER BY [LastElapsedTime(S)] * CONVERT(NUMERIC(12,4), ExecCount) DESC, [LastElapsedTime(S)] DESC 
+END
+	GO
